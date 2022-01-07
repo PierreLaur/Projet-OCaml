c1ef64abf385048c1f0b64246276ba0595dcbca3
PierreLaur <62428933+PierreLaur@users.noreply.github.com>
2022-01-07 14:01:34 +0100

    new branch

--- src/TriCount.ml
+++ src/TriCount.ml
@@ -3,38 +3,64 @@ open Gfile
 open Tools
 open FordFulkerson
 
-let create_graph laliste = 
+let create_graph listePersonnes = 
+  
   let moyenne =
     let rec aux liste =
       match liste with 
       | [] -> 0 
-      | (nom,montant)::rest -> (montant/(List.length liste)) + (moyenne rest)
+      | (nom,montant)::reste -> (montant/(List.length liste)) + (moyenne reste)
     in
-    aux laliste
+    aux listePersonnes
   in
 
-  let rec itere liste graphe num=
+  let rec createNodesAndAux liste graphe num=
     match (liste) with
-    | [] -> empty_graph
-    | (nom,montant)::rest -> if ((moyenne-montant)>0) then 
+    | [] -> graphe
+    | (nom,montant)::reste -> 
         (* pour chaque personne, on crée :
-           - une node numérotée num
-           - une node auxiliaire (source/puits) numérotée num+1
-           - un arc depuis/vers num représentant l'écart avec la moyenne *)
-        itere rest (new_arc    (new_node     (new_node graphe num)    (num+1))     (num+1) num (moyenne-montant) ) (num+2) 
-      else itere rest (new_arc    (new_node     (new_node graphe num)    (num+1))     num (num+1) (montant-moyenne) ) (num+2) 
+           - une node numérotée num *)
+      let nodeNum = new_node graphe num
+        (* - une node auxiliaire (source/puits) numérotée num+1 *)
+      in let nodeAux = new_node nodeNum (num+1)
+        (* - un arc depuis/vers num représentant l'écart avec la moyenne *)
+      in let arcAux = 
+        if ((moyenne-montant)>0) then 
+          new_arc nodeAux (num+1) num (moyenne-montant)
+        else
+          new_arc nodeAux num (num+1) (montant-moyenne)
+      in createNodesAndAux reste arcAux (num+2) 
 
   in
 
 
-  let arcs_internes graphe = 
-    (*val n_fold: 'a graph -> ('b -> id -> 'b) -> 'b -> 'b) *)
-    (*val new_arc: 'a graph -> id -> id -> 'a -> 'a graph *)
-    let rec aux2 gr (new_arc id) =
-      aux2 gr 
+  let createInternalArcs graphe = 
+    (* val n_fold: 'a graph -> ('b -> id -> 'b) -> 'b -> 'b *)
+    (* val new_arc: 'a graph -> id -> id -> 'a -> 'a graph *)
 
+    (* pour un node idFrom dans un graphe donné, createArcsFrom crée des arcs vers les autres nodes*)
+    (* on exclut les id impairs (nodes auxiliaires) *)
+    let createArcsFrom gr idFrom =
+
+      let createOutArc gr2 idTo =
+        if (idTo mod 2==0) then
+          new_arc gr2 idFrom idTo 0
+      in
+      
+      if (idFrom mod 2==0) then
+        (* on crée un arc sortant depuis idFrom vers tous les nodes *)
+        n_fold gr createOutArc gr
     in
-    n_fold graphe aux2 graphe 
+
+
+    (* on applique createArcsFrom sur tous les nodes *)
+    n_fold graphe createArcsFrom graphe 
   in
+
+
+  let createdGraph = createNodesAndAux listePersonnes empty_graph 0
+  in
+  let createdGraph = createInternalArcs createdGraph
+in createdGraph
 
 
